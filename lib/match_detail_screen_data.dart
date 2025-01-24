51176e88cf9e3594e088f45abebdb4f87c11ae55
Luis Pereira <luispereira21@live.com.pt>
2025-01-24 15:36:38 +0000

    modified:   lib/match_detail_screen_data.dart 	modified:   lib/patterns.dart 	new file:   lib/widgets/live_match_stats.dart

--- lib/match_detail_screen_data.dart
+++ lib/match_detail_screen_data.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+
 import 'package:iconsax/iconsax.dart';
 import 'package:soccer_live/model/live_match_model.dart';
 import 'package:soccer_live/patterns.dart';
-import 'package:soccer_live/widgets/live_match_detail.dart';
+import 'package:soccer_live/widgets/live_match_stats.dart';
+
+import '../Widgets/live_match_detail.dart';
 
 class MatchDetailScreen extends StatefulWidget {
   final LiveMatch liveMatch;
@@ -13,27 +16,23 @@ class MatchDetailScreen extends StatefulWidget {
 }
 
 class _MatchDetailScreenState extends State<MatchDetailScreen> {
-
-  List<Map<String,dynamic>> tabs = [
+  List<Map<String, dynamic>> tabs = [
     {
-      "label":"Stats",
+      "label": "Stats",
     },
     {
-      "label":"H2H",
+      "label": "H2H",
     },
     {
-      "label":"Table",
-    }
+      "label": "Table",
+    },
   ];
-
   int selectedIndex = 0;
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: detailBackgroundColor,
-      appBar: hearderPart(context),
+      appBar: headerParts(context),
       body: SingleChildScrollView(
         child: Stack(
           children: [
@@ -41,46 +40,110 @@ class _MatchDetailScreenState extends State<MatchDetailScreen> {
               margin: const EdgeInsets.only(top: 120),
               padding: const EdgeInsets.only(top: 180, right: 20, left: 20),
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
-                boxShadow: [
-                  BoxShadow(color: Colors.grey.shade300, blurRadius: 40),
-                ]
-
-              ),
-              child:  Column(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(30),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade300,
+                      blurRadius: 40,
+                    )
+                  ]),
+              child: Column(
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: List.generate(tabs.length, (index) {
-                      final tab = tabs[index];
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: selectedIndex == index ? kprimaryColor : detailBackgroundColor,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        margin: const EdgeInsets.only(right: 10),
-                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        child: Text(tab['label'], style: TextStyle(fontSize: 16, color: selectedIndex == index? Colors.white : Colors.grey),),
-                      );
-                    }),
+                    children: List.generate(
+                      tabs.length,
+                      (index) {
+                        final tab = tabs[index];
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: selectedIndex == index
+                                ? kprimaryColor
+                                : detailBackgroundColor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          margin: const EdgeInsets.only(right: 10),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                          child: Text(
+                            tab["label"],
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: selectedIndex == index
+                                    ? Colors.white
+                                    : Colors.grey),
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                  
-
+                  const SizedBox(height: 20),
+                  LiveMatchStats(
+                    homeStats: widget.liveMatch.shotOnTarget,
+                    awayStats: widget.liveMatch.shotOnTarget *2,
+                    title: "Shots  On Target",
+                    homeValue: widget.liveMatch.shotOnTarget.toDouble()/10,
+                    awayValue: widget.liveMatch.shotOnTarget.toDouble()*2/10,
+                    isHomeWinner: false,
+                  ),
+                   LiveMatchStats(
+                    homeStats: widget.liveMatch.homeGoal,
+                    awayStats: widget.liveMatch.awayGoal,
+                    title: "Goals",
+                    homeValue: widget.liveMatch.homeGoal.toDouble() / 5,
+                    awayValue: widget.liveMatch.awayGoal.toDouble() * 2 / 5,
+                    isHomeWinner: true,
+                  ),
+                  LiveMatchStats(
+                    homeStats: widget.liveMatch.possession,
+                    awayStats: 100 - widget.liveMatch.possession,
+                    title: "Possession",
+                    homeValue:
+                        widget.liveMatch.possession.toDouble() * 1.1 / 100,
+                    awayValue: widget.liveMatch.possession.toDouble() / 100,
+                    isHomeWinner: true,
+                  ),
+                  LiveMatchStats(
+                    homeStats: widget.liveMatch.yelloCard,
+                    awayStats: widget.liveMatch.yelloCard + 1,
+                    title: "Yellow Cards",
+                    homeValue: widget.liveMatch.yelloCard.toDouble() / 10,
+                    awayValue: widget.liveMatch.yelloCard.toDouble() * 2 / 10,
+                    isHomeWinner: false,
+                  ),
+                  LiveMatchStats(
+                    homeStats: widget.liveMatch.redCard - 1,
+                    awayStats: widget.liveMatch.redCard,
+                    title: "Red Cards",
+                    homeValue: widget.liveMatch.redCard.toDouble() / 10,
+                    awayValue: widget.liveMatch.redCard.toDouble() * 2 / 10,
+                    isHomeWinner: false,
+                  ),
+                  LiveMatchStats(
+                    homeStats: widget.liveMatch.corner,
+                    awayStats: widget.liveMatch.corner - 1,
+                    title: "Corner Kicks",
+                    homeValue: widget.liveMatch.redCard.toDouble() / 5,
+                    awayValue: widget.liveMatch.redCard.toDouble() / 5,
+                    isHomeWinner: true,
+                  ),
                 ],
               ),
             ),
-            const SizedBox(width: 20,),
-            
             LiveMatchDetail(widget: widget),
-
           ],
         ),
       ),
     );
   }
 
-  AppBar hearderPart(BuildContext context) {
+  AppBar headerParts(BuildContext context) {
     return AppBar(
       automaticallyImplyLeading: false,
       centerTitle: true,
@@ -98,19 +161,38 @@ class _MatchDetailScreenState extends State<MatchDetailScreen> {
                 borderRadius: BorderRadius.circular(10),
               ),
               padding: const EdgeInsets.all(8),
-              child: GestureDetector(onTap: (){ Navigator.pop(context);},child: const Icon(Iconsax.arrow_square_left )),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: const Icon(Iconsax.arrow_square_left),
+              ),
             ),
-          ),),
-          const Spacer(),
-          const Column(
-            children: [
-              Text("Champions League", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, letterSpacing: -1),),
-              Text("GROUP STAGE", style: TextStyle(fontSize: 14, color: Colors.grey, letterSpacing: -1),),
-
-            ],
           ),
-          const Spacer(),
-          Padding(
+        ),
+        const Spacer(),
+        const Column(
+          children: [
+            Text(
+              "Champions League",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                letterSpacing: -1,
+              ),
+            ),
+            Text(
+              "GROUP STAGE",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+                letterSpacing: -1,
+              ),
+            )
+          ],
+        ),
+        const Spacer(),
+        Padding(
           padding: const EdgeInsets.all(5),
           child: Material(
             elevation: 1,
@@ -123,8 +205,9 @@ class _MatchDetailScreenState extends State<MatchDetailScreen> {
               padding: const EdgeInsets.all(8),
               child: const Icon(Iconsax.more_square),
             ),
-          ),),
-          const SizedBox(width: 20,),
+          ),
+        ),
+        const SizedBox(width: 20),
       ],
     );
   }
